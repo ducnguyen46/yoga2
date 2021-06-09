73038b8561a8fdc0acab34aac2b4d96ef1a9eb1d
Nguyễn Ngọc Đức <nguyenngocduccl@gmail.com>
2021-06-09 15:43:16 +0700

    no message

--- lib/modules/dashboard/widgets/yoga_category_card.dart
+++ lib/modules/dashboard/widgets/yoga_category_card.dart
@@ -11,79 +11,117 @@ class YogaCategoryCard extends StatelessWidget {
   const YogaCategoryCard({Key key, @required this.category}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 263,
-      margin: EdgeInsets.only(left: 25),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        // color: Colors.green,
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              ),
-              child: Image.asset(
-                AppPath.toAssetsCategoryCard + '${category.image}',
-                width: 204,
-                height: 171,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Container(
-              width: 204,
-              height: 89,
-              decoration: BoxDecoration(
-                color: AppColor.reallyWhite,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ExerciseListScreen(
+                      category: category,
+                    )));
+      },
+      child: Container(
+        height: 263,
+        margin: EdgeInsets.only(left: 25),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          // color: Colors.green,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipRRect(
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: AppColor.grayBlue.withOpacity(0.4),
-                    offset: Offset(0, 4),
-                    blurRadius: 5,
-                  ),
-                ],
+                child: Image.asset(
+                  AppPath.toAssetsCategoryCard + '${category.image}',
+                  width: 204,
+                  height: 171,
+                  fit: BoxFit.fill,
+                ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(7, 7, 7, 7),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '${category.name}',
-                      style: TextStyle(
-                        color: AppColor.blueDark,
-                        fontSize: 19,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'GT',
-                      ),
-                      overflow: TextOverflow.ellipsis,
+              Container(
+                width: 204,
+                height: 89,
+                decoration: BoxDecoration(
+                  color: AppColor.reallyWhite,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColor.grayBlue.withOpacity(0.4),
+                      offset: Offset(0, 4),
+                      blurRadius: 5,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            //
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8, bottom: 2),
-                              child: Row(
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(7, 7, 7, 7),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${category.name}',
+                        style: TextStyle(
+                          color: AppColor.blueDark,
+                          fontSize: 19,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'GT',
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 8, bottom: 2),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      AppPath.toAssetsIcons + 'clock.svg',
+                                      semanticsLabel: 'Clock',
+                                      height: 20,
+                                      width: 20,
+                                      color: AppColor.purpleDecor,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 7),
+                                      child: Text(
+                                        '${category.count / 2} minutes',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                          fontFamily: 'GT',
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+
+                              //
+                              Row(
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
                                   SvgPicture.asset(
-                                    AppPath.toAssetsIcons + 'clock.svg',
-                                    semanticsLabel: 'Clock',
+                                    AppPath.toAssetsIcons + 'burn.svg',
+                                    semanticsLabel: 'Burn',
                                     height: 20,
                                     width: 20,
                                     color: AppColor.purpleDecor,
@@ -91,7 +129,7 @@ class YogaCategoryCard extends StatelessWidget {
                                   Padding(
                                     padding: const EdgeInsets.only(left: 7),
                                     child: Text(
-                                      '${category.count / 2} minutes',
+                                      '${category.count} exercises',
                                       style: TextStyle(
                                         color: Colors.black,
                                         fontSize: 15,
@@ -99,75 +137,37 @@ class YogaCategoryCard extends StatelessWidget {
                                         fontWeight: FontWeight.w400,
                                       ),
                                     ),
-                                  )
+                                  ),
                                 ],
                               ),
-                            ),
-
-                            //
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SvgPicture.asset(
-                                  AppPath.toAssetsIcons + 'burn.svg',
-                                  semanticsLabel: 'Burn',
-                                  height: 20,
-                                  width: 20,
-                                  color: AppColor.purpleDecor,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 7),
-                                  child: Text(
-                                    '${category.count} exercises',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontFamily: 'GT',
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-
-                        // Start Button
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ExerciseListScreen(
-                                          category: category,
-                                        )));
-                          },
-                          splashColor: AppColor.blueDark.withOpacity(0.8),
-                          child: Container(
-                            padding: EdgeInsets.all(6),
-                            decoration: BoxDecoration(
-                              color: AppColor.purpleDecor,
-                              borderRadius: BorderRadius.circular(9),
-                            ),
-                            child: Text(
-                              'Start',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                fontFamily: 'GT',
-                              ),
-                            ),
+                            ],
                           ),
-                        )
-                      ],
-                    ),
-                  ],
+
+                          // Start Button
+                          // Container(
+                          //   padding: EdgeInsets.all(6),
+                          //   decoration: BoxDecoration(
+                          //     color: AppColor.purpleDecor,
+                          //     borderRadius: BorderRadius.circular(9),
+                          //   ),
+                          //   child: Text(
+                          //     'Start',
+                          //     style: TextStyle(
+                          //       color: Colors.white,
+                          //       fontSize: 14,
+                          //       fontWeight: FontWeight.w500,
+                          //       fontFamily: 'GT',
+                          //     ),
+                          //   ),
+                          // )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
